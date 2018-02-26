d22d473819691d6423f032898fae40ddc1079def
andrewrlonsdale <andrewrlonsdale@hotmail.com>
2018-02-26 22:50:24 +0000

    add contact page

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,15 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/about', function(req, res, next) {
-  res.render('about', { title: 'Code4share - a platform for sharing code' });
+  res.render('about', { title: 'Collaborator - a platform for sharing code' });
+});
+
+router.route('/contact')
+.get(function(req, res, next) {
+  res.render('contact', { title: 'Collaborator - a platform for sharing code' });
+})
+.post(function(req, res, next) {
+  res.render('thank', { title: 'Collaborator - a platform for sharing code' });
 });
 
 module.exports = router;
