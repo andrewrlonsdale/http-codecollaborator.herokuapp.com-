cf45f7290086da4c105289b7f802393dd4157b8c
Andrew Lonsdale <andrewlonsdale@Andrews-MacBook-Air.local>
2018-02-26 16:20:04 +0000

    add nav bar

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Collaborator' });
 });
 
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'Code4share - a platform for sharing code' });
+});
+
 module.exports = router;
