9439a5b3ddeef694b18e1f1d8dcff26c687840cc
andrewrlonsdale <andrewrlonsdale@hotmail.com>
2018-02-28 11:31:26 +0000

    add login and register page

---
+++ config.js
@@ -0,0 +1,12 @@
+'use strict'
+
+module.exports = {
+  mailer: {
+    service: 'Gmail',
+    auth: {
+      user: 'andrewrlonsdale92@gmail.com',
+      pass: 'Thumper12'
+
+    }
+  }
+}
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
+var nodemailer = require('nodemailer');
+var config = require('../config')
+var transporter = nodemailer.createTransport(config.mailer);
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
 	res.render('index', {title: 'Collaborator'});
@@ -27,8 +31,30 @@ router.route('/contact').get(function(req, res, next) {
 			errorMessages: errors
 		});
 	} else {
-		res.render('thank', {title: 'Collaborator - a platform for sharing code'});
+		var mailOptions = {
+			from: req.body.email,
+			to: 'andrewrlonsdale92@gmail.com',
+			subject: 'You got a new message from visitor',
+			text:req.body.message
+		};
+
+		transporter.sendMail(mailOptions, function(error, info) {
+			if (error) {
+				return console.log(error);
+			}
+			res.render('thank', {title: 'Collaborator - a platform for sharing code'});
+		});
+
 	}
 });
+
+router.get('/login', function(req, res, next) {
+	res.render('login', {title: 'Login to your account'});
+});
+
+router.get('/register', function(req, res, next) {
+	res.render('register', {title: 'Register a new account'});
+});
+
 
 module.exports = router;
