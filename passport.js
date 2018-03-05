e6694265f348891cebc856435ba1f7b817c601cd
andrewrlonsdale <andrewrlonsdale@hotmail.com>
2018-03-05 01:00:27 +0000

    add facebook authentication

--- passport.js
+++ passport.js
@@ -1,34 +1,71 @@
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
-
-passport.serializeUser(function (user, done) {
-  done(null, user._id);
+var FacebookStrategy = require('passport-facebook').Strategy;
+passport.serializeUser(function(user, done) {
+	done(null, user._id);
 });
 
-passport.deserializeUser(function (id, done) {
-  User.findOne({_id: id}, function (err, user) {
-    done(err, user);
-  })
+passport.deserializeUser(function(id, done) {
+	User.findOne({
+		_id: id
+	}, function(err, user) {
+		done(err, user);
+	})
 });
 
 passport.use(new LocalStrategy({
-    usernameField: 'email'
-  },
-  function (username, password, done) {
-    User.findOne({email: username}, function (err, user) {
+	usernameField: 'email'
+}, function(username, password, done) {
+	User.findOne({
+		email: username
+	}, function(err, user) {
+		if (err)
+			return done(err);
+		if (!user) {
+			return done(null, false, {message: 'Incorrect username or password'});
+		}
+		if (!user.validPassword(password)) {
+			return done(null, false, {message: 'Incorrect username or password'});
+		}
+
+		return done(null, user);
+	})
+}));
+
+passport.use(new FacebookStrategy({
+	clientID: '352659715216894',
+	clientSecret: '3be7f8b3de7621f42462510f2950e90e',
+	callbackURL: 'http://localhost:3000/auth/facebook/callback',
+	profileFields: ['id', 'displayName', 'email']
+},
+function(token, refreshToken, profile, done) {
+    User.findOne({'facebookId': profile.id}, function(err, user) {
       if (err) return done(err);
-      if (!user) {
-        return done(null, false, {
-          message: 'Incorrect username or password'
-        });
-      }
-      if (!user.validPassword(password)) {
-        return done(null, false, {
-          message: 'Incorrect username or password'
-        });
+
+      if (user) {
+        return done(null, user);
+      } else {
+        User.findOne({email: profile.emails[0].value}, function (err, user) {
+          if (user) {
+            user.facebookId = profile.id
+            return user.save(function (err) {
+              if (err) return done(null, false, { message: "Can't save user info"});
+              return done(null, user);
+            })
+          }
+
+          var user = new User();
+          user.name = profile.displayName;
+          user.email = profile.emails[0].value;
+          user.facebookId = profile.id
+          user.save(function (err) {
+            if (err) return done(null, false, { message: "Can't save user info"});
+            return done(null, user);
+          });
+        })
       }
 
-      return done(null, user);
-    })
+
+    });
   }
 ));
